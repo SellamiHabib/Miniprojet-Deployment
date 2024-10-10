b31dd2747f8d04bea2adc0117b06615a9525d7bb
Habib <habib.sellami.personal@gmail.com>
2024-10-10 17:02:02 +0100

    feat(rateLimit): add initial rate limit middleware

---
+++ src/middlewares/rateLimit.middleware.ts
@@ -0,0 +1,25 @@
+import { NextFunction, Request, Response } from 'express';
+import TokenService from '../services/auth.service';
+import { StatusCodes } from 'http-status-codes';
+import { JustifyRequestInputDTO } from '../dtos/requests/justify.requests';
+
+const MAX_WORDS_PER_DAY = 80000;
+
+export const rateLimitMiddleware = (req: Request<unknown, unknown, JustifyRequestInputDTO>, res: Response, next: NextFunction) => {
+  const text = req.body.text;
+  const token = res.locals.token;
+
+  if (!text) {
+    res.status(StatusCodes.BAD_REQUEST).json({ error: 'Text is required' });
+  }
+
+  const tokenDetails = TokenService.getTokenDetails(token);
+  const wordCount = text.split(/\s+/).length;
+
+  if (tokenDetails && (tokenDetails.wordCount + wordCount) > MAX_WORDS_PER_DAY) {
+    res.status(StatusCodes.PAYMENT_REQUIRED).json({ error: 'Word limit exceeded' });
+  }
+
+  TokenService.updateTokenWordCount(token, wordCount);
+  next();
+};

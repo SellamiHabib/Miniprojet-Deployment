e82e190fb5c06c9fd58651cdffeb1f9628e8abbe
Habib <habib.sellami.personal@gmail.com>
2024-10-10 10:47:13 +0100

    fix(env): fix env variables not being read correctly

--- env.ts
+++ env.ts
@@ -1,12 +1,15 @@
 import { createEnv } from './src/utils/envValidator';
 import { z } from 'zod';
-import process from 'node:process';
+
+
 export const env = createEnv({
   envSchema: z.object({
     PORT: z.string().min(1),
+    APP_URL: z.string().min(1),
   }),
   runtimeValues: {
     PORT: process.env.PORT,
+    APP_URL: process.env.APP_URL,
   },
   skipValidation: false,
 });
---
+++ index.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import { env } from './env';
+import bodyParser from 'body-parser';
+import { StatusCodes } from 'http-status-codes';
+import { swaggerDocs } from './src/config/swaggerConfig';
+import swaggerUi from 'swagger-ui-express';
+import justifyRouter from './src/routes/justify.router';
+import authRouter from './src/routes/auth.router';
+
+const app = express();
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+const port = env.PORT || 3000;
+app.use(bodyParser.text());
+app.use(bodyParser.json()); // For token request parsing
+
+
+app.use("/api",justifyRouter);
+app.use("/api",authRouter);
+app.get('/healthcheck', async (_req, res) => {
+  res.status(StatusCodes.OK).send({
+    status: 'ok',
+  });
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});

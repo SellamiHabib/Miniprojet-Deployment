9ec28d31f4aae5f5d9107b6ed10bfa46db44433e
Habib <habib.sellami.personal@gmail.com>
2024-10-11 18:37:44 +0100

    refactor(middlewares): fix rate middleware not using newly refactored classes

--- src/middlewares/rateLimit.middleware.ts
+++ src/middlewares/rateLimit.middleware.ts
@@ -1,25 +1,26 @@
 import { NextFunction, Request, Response } from 'express';
-import TokenService from '../services/auth.service';
+import AuthService from '../services/auth.service';
 import { StatusCodes } from 'http-status-codes';
 import { JustifyRequestInputDTO } from '../dtos/requests/justify.requests';
 
 const MAX_WORDS_PER_DAY = 80000;
 
-export const rateLimitMiddleware = (req: Request<unknown, unknown, JustifyRequestInputDTO>, res: Response, next: NextFunction) => {
+export const rateLimitMiddleware = async (req: Request<unknown, unknown, JustifyRequestInputDTO>, res: Response, next: NextFunction) => {
   const text = req.body;
-  const token = res.locals.token;
+  const user = res.locals.user;
 
   if (!text) {
     res.status(StatusCodes.BAD_REQUEST).json({ error: 'Text is required' });
   }
 
-  const tokenDetails = TokenService.getTokenDetails(token);
+  const userDetails = await AuthService.getUserDetails(user.email);
   const wordCount = text.split(/\s+/).length;
 
-  if (tokenDetails && (tokenDetails.wordCount + wordCount) > MAX_WORDS_PER_DAY) {
+  if (userDetails && (userDetails.wordCount + wordCount) > MAX_WORDS_PER_DAY) {
     res.status(StatusCodes.PAYMENT_REQUIRED).json({ error: 'Word limit exceeded' });
   }
 
-  TokenService.updateTokenWordCount(token, wordCount);
+  AuthService.updateWordCount(userDetails.email, wordCount);
   next();
+  return;
 };

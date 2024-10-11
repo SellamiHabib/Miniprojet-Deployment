8db058c40770b403f68bec46429736b520e197e9
Habib <habib.sellami.personal@gmail.com>
2024-10-11 22:01:33 +0100

    fix(schemas): fix dates not being able be parsed when read from database

--- src/schemas/base.schema.ts
+++ src/schemas/base.schema.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 
 export const baseSchema = z.object({
   id: z.string(),
-  createdAt: z.date().default(new Date()),
-  updatedAt: z.date().default(new Date()),
+  createdAt: z.coerce.date().default(new Date()),
+  updatedAt: z.coerce.date().default(new Date()),
 });
 export type BaseModel = z.infer<typeof baseSchema>;
